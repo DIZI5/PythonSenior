b733e4ce69da8c7deb6fb83478bfcbb159dba502
unknown <qweadszxc88@gmail.com>
2022-04-07 17:06:11 +0300

    Calculator

---
+++ 2.Lesson/Start.py
@@ -0,0 +1,39 @@
+# Перший код.
+from re import A
+from unicodedata import name
+
+
+print("Hello World!")
+
+first = 23
+print(first)
+
+second = "Number"
+print(second)
+
+print(first,second)
+
+a = 10 + 5
+b = a + 20
+c = (a + b) * 2
+print(a,b,c)
+
+first_string = "Hello World!"
+second_string = 'Hello World!'
+third_string = """"Hello World!"""
+
+name = input('Ваше Ім`я?')
+
+greeting = "Привіт, " + name
+
+print(greeting)
+
+string = input('Напишіть любий текст: ')
+
+string_length = len(string)
+print(string_length)
+
+result = 10+15
+
+text = "Результат = " + str(result)
+print(text)
