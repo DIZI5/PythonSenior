86eb99484ef25d2d5e9a563d97aded49e2d8e934
unknown <qweadszxc88@gmail.com>
2022-04-24 23:10:45 +0300

    SIMS 4

---
+++ 7.Lesson/Lesson 7.py
@@ -0,0 +1,44 @@
+import random
+
+class Human:
+    def __init__(self,name = "Human",job = None , home = None, car = None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.setiety = 50
+        self.job = job
+        self.home = home
+        self.car = car
+    
+    def get_home(self):
+        pass
+    
+    def get_car(self):
+        pass
+
+    def get_job(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def work(self):
+        pass
+
+    def shopping(self,manage):
+        pass
+
+    def chill(self):
+        pass
+
+    def clean_home(self):
+        pass
+
+    def days_index(self,day):
+        pass
+
+    def is_alive(self):
+        pass
+
+    def live(self,day):
+        pass
