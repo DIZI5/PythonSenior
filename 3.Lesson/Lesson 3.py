decff1fd97edbaf23751c5ae0a73a425aebe8913
unknown <qweadszxc88@gmail.com>
2022-04-27 20:13:01 +0300

    DZ 3

---
+++ 3.Lesson/Lesson 3.py
@@ -0,0 +1,25 @@
+# number = input('Введи число від-до: ')
+# sum = 0
+# for i in range(int(number)):
+#     print(i*2)
+
+
+# ---------------1---------------
+# number = input('Введи число від-до: ')
+# sum = 0
+# for i in range(int(number)):
+#     print(i*i)
+
+
+# number = input('Введи число від-до: ')
+# sum = 0
+# for i in range(int(number)):
+#     sum = sum + 1
+#     print(sum)
+
+
+number = input('Введи число від-до: ')
+sum = 0
+for i in range(int(number)):
+    sum = sum + i*i
+    print("Suma = ", str(sum))
